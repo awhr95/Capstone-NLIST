6149ee7105709ccd93d6186a314b41f11053667a
awhr95 <awhr95@gmail.com>
2024-01-28 22:52:39 +0000

    Add log in page

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,10 +1,49 @@
 import "./LoginPage.scss";
+import Input from "../../components/Input/Input";
+import { useNavigate, Link } from "react-router-dom";
+import axios from "axios";
+import { useState } from "react";
+import nlistLogo from "../../assets/logos/NList-logos.jpeg";
 
 const LoginPage = () => {
+  const navigate = useNavigate();
+
+  const [error, setError] = useState("");
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await axios.post(
+        "http://localhost:5050/api/users/login",
+        {
+          email: event.target.email.value,
+          password: event.target.password.value,
+        }
+      );
+
+      sessionStorage.setItem("token", response.data.token);
+
+      navigate("/profile");
+    } catch (error) {
+      setError(error.response.data);
+    }
+  };
   return (
-    <>
-      <h1>Hello NLIST</h1>
-    </>
+    <main className="login">
+      <header className="login__header">
+        <img className="login__logo" src={nlistLogo}></img>
+      </header>
+      <form className="login__form" onSubmit={handleSubmit}>
+        <h1 className="login__title">Log in</h1>
+        <Input type="text" name="email" label="Email" />
+        <Input type="password" name="password" label="Password" />
+        <button className="login__button">Log in</button>
+        {error && <div className="login__message">{error}</div>}
+      </form>
+      <p>
+        Need an account? <Link to="/signup">Sign up</Link>
+      </p>
+    </main>
   );
 };
 
---
+++ src/pages/SignupPage/SignupPage.jsx
@@ -0,0 +1,49 @@
+import Input from "../../components/Input/Input";
+import "./Signup.scss";
+import axios from "axios";
+import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
+
+function Signup() {
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      await axios.post("http://localhost:5050/api/users/register", {
+        email: event.target.email.value,
+        password: event.target.password.value,
+        first_name: event.target.first_name.value,
+        last_name: event.target.last_name.value,
+        phone: event.target.phone.value,
+        address: event.target.address.value,
+      });
+      navigate("/login");
+    } catch (error) {
+      event.target.reset();
+      setError(error.response.data);
+    }
+  };
+
+  return (
+    <main className="signup-page">
+      <form className="signup" onSubmit={handleSubmit}>
+        <h1 className="signup__title">Sign up</h1>
+        <Input type="text" name="first_name" label="First name" />
+        <Input type="text" name="last_name" label="Last name" />
+        <Input type="text" name="phone" label="Phone" />
+        <Input type="text" name="address" label="Address" />
+        <Input type="text" name="email" label="Email" />
+        <Input type="password" name="password" label="Password" />
+        <button className="signup__button">Sign up</button>
+        <p>{error}</p>
+      </form>
+      <p>
+        Have an account? <Link to="/login">Log in</Link>
+      </p>
+    </main>
+  );
+}
+
+export default Signup;
