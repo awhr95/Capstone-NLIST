0801d89f00d8970327b30f018b37803aa6282fbf
awhr95 <awhr95@gmail.com>
2024-02-04 11:43:33 +0000

    Add mobile styling to opportunity cards

--- src/components/Opportunities/Opportunities.jsx
+++ src/components/Opportunities/Opportunities.jsx
@@ -2,9 +2,18 @@ import "./Opportunities.scss";
 import { Link } from "react-router-dom";
 
 function Opportunity({ opportunities }) {
+  function timeFormatter(timeString) {
+    let startTime = new Date(`2024-01-01T${timeString}`);
+    let formattedTime = startTime.toLocaleTimeString("en-US", {
+      hour: "numeric",
+      minute: "2-digit",
+      hour12: true,
+    });
+    return formattedTime;
+  }
   return (
-    <section>
-      <ul>
+    <section className="opportunity">
+      <ul className="opportunity__list">
         {opportunities.map((opportunity) => {
           const formattedDate = new Date(
             opportunity.date_of_opportunity
@@ -14,11 +23,27 @@ function Opportunity({ opportunities }) {
             day: "numeric",
           });
           return (
-            <li key={opportunity.id}>
-              <Link to={`/opportunity/${opportunity.id}`}>
-                <h2>{opportunity.title}</h2>
-                <p>{opportunity.description}</p>
-                <p>{formattedDate}</p>
+            <li key={opportunity.id} className="opportunity__card">
+              <Link
+                className="opportunity__link"
+                to={`/opportunity/${opportunity.id}`}
+              >
+                <h2 className="opportunity__title">{opportunity.title}</h2>
+                <p className="opportunity__descpription">
+                  {opportunity.description}
+                </p>
+                <div className="opportunity__details">
+                  <p className="opportunity__date">{formattedDate}</p>
+                  <div className="opportunity__container">
+                    <p className="opportunity__time">
+                      {`${timeFormatter(
+                        opportunity.start_time_of_opportunity
+                      )} to ${timeFormatter(
+                        opportunity.end_time_of_opportunity
+                      )}`}
+                    </p>
+                  </div>
+                </div>
               </Link>
             </li>
           );
