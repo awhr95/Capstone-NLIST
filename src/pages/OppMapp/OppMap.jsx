c6bee059d367e1af2e33a3d0ca806159ffa66d8c
awhr95 <awhr95@gmail.com>
2024-02-05 15:35:17 +0000

    Add env details

--- src/pages/OppMapp/OppMap.jsx
+++ src/pages/OppMapp/OppMap.jsx
@@ -3,16 +3,29 @@ import Header from "../../components/Header/Header";
 import FooterNav from "../../components/FooterNav/FooterNav";
 import FilterMenu from "../../components/FilterMenu/FilerMenu";
 import { useRef, useState, useEffect } from "react";
+import axios from "axios";
 import mapboxgl from "mapbox-gl";
 
 mapboxgl.accessToken = process.env.REACT_APP_MAP_BOX_TOKEN;
 
 function OppMap() {
+  const apiUrl = process.env.REACT_APP_URL;
+  const port = process.env.REACT_APP_PORT;
   const mapContainer = useRef(null);
   const map = useRef(null);
   const [lng, setLng] = useState(-1.72);
   const [lat, setLat] = useState(51.5);
   const [zoom, setZoom] = useState(5);
+  const [allOpportunities, setAllOpportunities] = useState(null);
+
+  const fetchOpportunities = async () => {
+    try {
+      const response = await axios.get(`${apiUrl}:${port}/opportunities`);
+      setAllOpportunities(response.data);
+    } catch (error) {
+      console.error(error.message);
+    }
+  };
 
   useEffect(() => {
     if (map.current) return; // initialize map only once
@@ -27,6 +40,8 @@ function OppMap() {
       setLat(map.current.getCenter().lat.toFixed(4));
       setZoom(map.current.getZoom().toFixed(2));
     });
+
+    fetchOpportunities();
   }, []);
 
   return (
