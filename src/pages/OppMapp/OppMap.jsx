53843b3c407371d5f4c43e26d1afad08f2d35e54
awhr95 <awhr95@gmail.com>
2024-02-06 17:17:56 +0000

    Add map-box with geojson data

--- src/pages/OppMapp/OppMap.jsx
+++ src/pages/OppMapp/OppMap.jsx
@@ -4,7 +4,7 @@ import FooterNav from "../../components/FooterNav/FooterNav";
 import FilterMenu from "../../components/FilterMenu/FilerMenu";
 import { useRef, useState, useEffect } from "react";
 import axios from "axios";
-import mapboxgl from "mapbox-gl";
+import mapboxgl, { clearStorage } from "mapbox-gl";
 
 mapboxgl.accessToken = process.env.REACT_APP_MAP_BOX_TOKEN;
 
@@ -17,6 +17,8 @@ function OppMap() {
   const [lat, setLat] = useState(51.45);
   const [zoom, setZoom] = useState(10);
   const [allOpportunities, setAllOpportunities] = useState(null);
+  const [locationData, setLocationData] = useState(null);
+  const [unpackedLocationData, setUnpackedLocationData] = useState(null);
 
   const fetchOpportunities = async () => {
     try {
@@ -26,9 +28,51 @@ function OppMap() {
       console.error(error.message);
     }
   };
+  const fetchMapData = async () => {
+    try {
+      const { data } = await axios.get(`${apiUrl}:${port}/map`);
+      setLocationData(data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  function flyToOpp(currentFeature) {
+    map.current.flyTo({
+      center: currentFeature.geometry.coordinates,
+      zoom: 14,
+    });
+  }
+  function buildLocationList(locationData) {
+    if (!locationData) {
+      return null;
+    }
+    return (
+      <div className="map__listings">
+        {locationData.map((feature, index) => (
+          <div key={index} className="map__item">
+            <a
+              onClick={() => {
+                flyToOpp(feature);
+              }}
+              href="#"
+              className="map__title"
+            >
+              {feature.properties.title}
+            </a>
+            <div className="map__details">{feature.properties.description}</div>
+          </div>
+        ))}
+      </div>
+    );
+  }
 
   useEffect(() => {
-    if (map.current) return; // initialize map only once
+    fetchOpportunities();
+    fetchMapData();
+  }, []);
+
+  useEffect(() => {
+    if (map.current || !locationData) return; // initialize map only once
     map.current = new mapboxgl.Map({
       container: mapContainer.current,
       style: "mapbox://styles/alex9595/cls92x1um01b201pl90gg0i2n",
@@ -40,43 +84,20 @@ function OppMap() {
       setLat(map.current.getCenter().lat.toFixed(4));
       setZoom(map.current.getZoom().toFixed(2));
     });
-    // map.current.on("click", (event) => {
-    //   // If the user clicked on one of your markers, get its information.
-    //   const features = map.current.queryRenderedFeatures(event.point, {
-    //     layers: ["seed-data"], // replace with your layer name
-    //   });
-    //   if (!features.length) {
-    //     return;
-    //   }
-    //   const feature = features[0];
-    //   console.log(feature);
 
-    //   // Code from the next step will go here.
-    //   const popup = new mapboxgl.Popup({ offset: [0, -15] })
-    //     .setLngLat(feature.geometry.coordinates)
-    //     .setHTML(
-    //       `<h3>${feature.properties.title}</h3><p>${feature.properties.description}</p>`
-    //     );
-    //   // Log the map object to the console to verify its structure
-    //   console.log(map);
-
-    //   // Check if the map is valid before adding the popup
-    //   if (map && typeof map.addControl === "function") {
-    //     popup.addTo(map);
-    //   }
-    // });
     map.current.on("click", (event) => {
       const features = map.current.queryRenderedFeatures(event.point, {
         layers: ["seed-data"], // replace with your layer name
       });
+      // console.log(features);
 
       if (!features.length) {
         return;
       }
 
       const feature = features[0];
+      flyToOpp(feature);
 
-      // Code from the next step will go here.
       const popup = new mapboxgl.Popup({ offset: [0, -15] })
         .setLngLat(feature.geometry.coordinates)
         .setHTML(
@@ -87,8 +108,29 @@ function OppMap() {
         popup.addTo(map.current);
       }
     });
-    fetchOpportunities();
-  }, []);
+
+    map.current.on("load", () => {
+      // Ensure locationData is not null before proceeding
+
+      if (locationData) {
+        map.current.addLayer({
+          id: "locations",
+          type: "circle",
+          source: {
+            type: "geojson",
+            data: locationData,
+          },
+        });
+        setUnpackedLocationData(locationData.features);
+      }
+    });
+  }, [locationData]);
+
+  let listings = null;
+
+  if (unpackedLocationData && unpackedLocationData.length > 0) {
+    listings = buildLocationList(unpackedLocationData);
+  }
 
   return (
     <>
@@ -97,7 +139,12 @@ function OppMap() {
       {/* <div className="sidebar">
         Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
       </div> */}
-      <div ref={mapContainer} className="map-container"></div>
+      {/* <div ref={mapContainer} className="map-container"></div> */}
+      <div className="map__page">
+        <section className="map__list-container">{listings}</section>
+        <div ref={mapContainer} className="map__container"></div>
+      </div>
+
       <FooterNav />
     </>
   );
